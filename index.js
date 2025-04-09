4e75f59415e0ee410b393d63c750fa331d27b604
NARNE UDAYA SREE <152365861+UdayaNarne@users.noreply.github.com>
2025-04-09 08:45:54 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("video").addEventListener("click",function(){
+    window.location.href = "page3.html";
+})
+
+document.getElementById("image").addEventListener("click",function(event){
+    window.location.href = "page2.html";
+});
